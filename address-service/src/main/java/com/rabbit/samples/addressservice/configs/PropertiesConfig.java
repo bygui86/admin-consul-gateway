124a12945df328b31556b1681c5126104612311f
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-03-24 14:44:59 +0100

    Added address service

---
+++ address-service/src/main/java/com/rabbit/samples/addressservice/configs/PropertiesConfig.java
@@ -0,0 +1,15 @@
+package com.rabbit.samples.addressservice.configs;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+/**
+ * @author Matteo Baiguini
+ * matteo@solidarchitectures.com
+ * 24 Mar 2019
+ */
+@EnableConfigurationProperties
+public class PropertiesConfig {
+
+	// no-op
+}
