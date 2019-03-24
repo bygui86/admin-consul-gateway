d1d4cf24b350b722da0e7521198069cb735ded03
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-03-24 14:45:06 +0100

    Added person service

---
+++ person-service/src/main/java/com/rabbit/samples/personservice/properties/CommonProperties.java
@@ -0,0 +1,27 @@
+package com.rabbit.samples.personservice.properties;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author Matteo Baiguini
+ * matteo@solidarchitectures.com
+ * 24 Mar 2019
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@RefreshScope
+@Configuration
+@ConfigurationProperties("common")
+public class CommonProperties {
+
+	String property;
+
+}
